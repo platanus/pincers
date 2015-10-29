affc67b027d15d28158f8cd20080efc960aaec6d
Ignacio Baixas <iobaixas@gmail.com>
2015-10-29 16:22:14 -0300

    fix(http.cookie_jar): fixes various RFC miss interpretations
    Also makes the Cookie structure inmutable

--- lib/pincers/http/cookie.rb
+++ lib/pincers/http/cookie.rb
@@ -1,3 +1,24 @@
 module Pincers::Http
-  class Cookie < Struct.new(:name, :value, :domain, :path, :expires, :secure); end
+  class Cookie
+
+    attr_reader :name, :value, :domain, :path, :expires, :secure
+
+    def initialize(_name, _value, _domain, _path=nil, _expires=nil, _secure=nil)
+      @name = _name
+      @value = _value
+      @domain = _domain.gsub(/^\./,'').downcase # RFC 6265 5.2.3
+      @path = valid_path?(_path) ? _path.downcase : '/'
+      @expires = _expires
+      @secure = _secure
+    end
+
+  private
+
+    def valid_path?(_path)
+      !_path.nil? && !_path.empty? && _path[0] == '/'
+    end
+
+  end
+
+
 end
--- spec/lib/http/cookie_jar_spec.rb
+++ spec/lib/http/cookie_jar_spec.rb
@@ -7,16 +7,19 @@ describe Pincers::Http::CookieJar do
   let(:base_uri) { URI.parse "http://crabfarm.io" }
   let(:internal_uri) { URI.parse "http://crabfarm.io/demo/" }
   let(:subdomain_uri) { URI.parse "http://blog.crabfarm.io" }
+  let(:similar_uri) { URI.parse "http://thecrabfarm.io" }
   let(:simple_cookie) { "NAME=VALUE;" }
-  let(:domain_cookie) { "NAME=VALUE; domain=www.crabfarm.io" }
+  let(:domain_cookie) { "NAME=VALUE; domain=blog.crabfarm.io;" }
   let(:other_domain_cookie) { "NAME=VALUE; domain=www.crabtrap.io" }
+  let(:cookie_w_comma) { "FOO=BAR, NAME=VALUE; Expires=Thu, 01-Jan-1970 00:00:10 GMT" }
 
   describe "set_raw" do
 
     it "should properly set the cookie domain" do
-      expect(jar.set_raw(base_uri, simple_cookie).domain).to eq("crabfarm.io")
-      expect(jar.set_raw(internal_uri, simple_cookie).domain).to eq("crabfarm.io")
-      expect(jar.set_raw(internal_uri, domain_cookie).domain).to eq("www.crabfarm.io")
+      expect(jar.set_raw(base_uri, simple_cookie).domain).to eq 'crabfarm.io'
+      expect(jar.set_raw(internal_uri, simple_cookie).domain).to eq 'crabfarm.io'
+      expect(jar.set_raw(subdomain_uri, domain_cookie).domain).to eq 'blog.crabfarm.io'
+      expect(jar.set_raw(internal_uri, domain_cookie)).to be nil
       expect(jar.set_raw(internal_uri, other_domain_cookie)).to be nil
     end
 
@@ -25,13 +28,18 @@ describe Pincers::Http::CookieJar do
 
   end
 
+  describe "set_from_header" do
+    it { expect(jar.set_from_header(base_uri, cookie_w_comma).count).to eq 2 }
+    it { expect(jar.set_from_header(base_uri, cookie_w_comma).last.name).to eq 'NAME' }
+  end
+
   context "given some already set cookies" do
 
     before do
-      jar.set domain: 'google.com', name: 'lastq', value: 'webscraping'
-      jar.set domain: 'blog.crabfarm.io', name: 'session', value: 'somesessiondata'
-      jar.set domain: '.crabfarm.io', name: 'session', value: 'othersessiondata'
-      jar.set domain: 'crabfarm.io', name: 'session', value: 'othersessiondata', path: '/demo'
+      jar.set Pincers::Http::Cookie.new('lastq', 'webscraping', 'google.com', '/')
+      jar.set Pincers::Http::Cookie.new('session','somesessiondata', 'blog.crabfarm.io', '/')
+      jar.set Pincers::Http::Cookie.new('session', 'othersessiondata', '.crabfarm.io', '/')
+      jar.set Pincers::Http::Cookie.new('session', 'othersessiondata', 'crabfarm.io', '/demo')
     end
 
     describe "for_origin" do
@@ -40,6 +48,7 @@ describe Pincers::Http::CookieJar do
         expect(jar.for_origin(internal_uri).count).to eq 2
         expect(jar.for_origin(base_uri).count).to eq 1
         expect(jar.for_origin(subdomain_uri).count).to eq 2
+        expect(jar.for_origin(similar_uri).count).to eq 0
       end
 
     end
@@ -48,14 +57,12 @@ describe Pincers::Http::CookieJar do
 
       it "should replace other cookies with same domain and name" do
         expect {
-          jar.set domain: 'crabfarm.io', name: 'session', value: 'empty'
+          jar.set Pincers::Http::Cookie.new('session', 'empty', '.crabfarm.io', '/')
         }.not_to change {
           jar.cookies.count
         }
       end
 
     end
-
   end
-
 end
